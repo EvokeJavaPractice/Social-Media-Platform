93a6e434024c775200d409b6cd928a23ce609ff6
Bhaskar Nelapogula <bnelapogula@evoketechnologies.com>
2023-06-26 12:43:31 +0530

    added swagger ui and documentation

--- src/main/java/spring/angular/social/SocialMediaPlatformApplication.java
+++ src/main/java/spring/angular/social/SocialMediaPlatformApplication.java
@@ -3,7 +3,10 @@ package spring.angular.social;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class SocialMediaPlatformApplication {
 
     public static void main(String[] args) {
