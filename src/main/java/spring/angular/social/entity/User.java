d6b32bda13ebec064e15c4de58c9e09a8484af2f
surya <spadala@evoketechnologies.com>
2023-07-21 18:41:01 +0530

    fix: login issue fixed

--- src/main/java/spring/angular/social/entity/User.java
+++ src/main/java/spring/angular/social/entity/User.java
@@ -1,6 +1,7 @@
 package spring.angular.social.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Setter
 @Entity
 @Table(name="social_user")
+@NoArgsConstructor
 public class User {
 
 		@Id
